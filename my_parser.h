5d65f0117ee58217034a0a4ef58964114c8f17de
Honda Kenshiro <48042661+Kenshiro-Honda@users.noreply.github.com>
2020-06-07 12:43:30 +0900

    separate types/funcs into header

---
+++ my_parser.h
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#ifndef _MY_PAESER
+#define _MY_PARSER
+
+typedef struct {
+  int year, month, day;
+  int hour, min, sec;
+  int timezone;
+} my_time;
+
+char month_name[12][4] = 
+  {"Jan","Feb","Mar","Apr","May","Jun",
+   "Jul","Aug","Sep","Oct","Nov","Dec"};
+
+typedef struct {
+  char host[16];
+  char client_id[32];
+  char username[32];
+  my_time time;
+  char request[256];
+  int status;
+  int res_size;
+  char referer[256];
+  char user_agent[256];
+} my_log;
+
+typedef struct {
+  char host[16];
+  int count;
+} host_counter;
+
+void debug(char*);
+void parse_log(char*, my_log*);
+void parse_time(char*, my_time*);
+void print_json(my_log);
+
+void parse_log(char* line, my_log* log) {
+  char time_str[256];
+  sscanf(line, "%s %s %s [%[^]]] \"%[^\"]\" %d %d \"%[^\"]\" \"%[^\"]\"", 
+    log->host, log->client_id, log->username, time_str,
+    log->request, &(log->status), &(log->res_size), 
+    log->referer, log->user_agent);
+  parse_time(time_str, &(log->time));
+}
+
+void parse_time(char* str, my_time* time) {
+  char month_str[4];
+  int i;
+  sscanf(str, "%d/%[^/]/%d:%d:%d:%d %d", 
+    &(time->day), month_str, &(time->year),
+    &(time->hour), &(time->min), &(time->sec),
+    &(time->timezone));
+  for ( i = 0; i < 12; i++ ) {
+    if ( strcmp(month_str,month_name[i]) == 0 ) {
+      time->month = i+1;
+      break;
+    }
+  }
+}
+
+void print_json(my_log log) {
+  printf("{\n");
+  printf("  \"host\":\"%s\",\n", log.host);
+  printf("  \"client_id\":\"%s\",\n", log.client_id);
+  printf("  \"time\":\"%d/%02d/%02d %02d:%02d:%02d %s%04d\",\n",
+    log.time.year, log.time.month, log.time.day,
+    log.time.hour, log.time.min, log.time.sec,
+    (log.time.timezone<0?"-":log.time.timezone>0?"+":""), abs(log.time.timezone));
+  printf("  \"request\":\"%s\",\n", log.request);
+  printf("  \"status\":%d,\n", log.status);
+  printf("  \"res_size\":%d,\n", log.res_size);
+  printf("  \"referer\":\"%s\",\n", log.referer);
+  printf("  \"user_agent\":\"%s\"\n", log.user_agent);
+  printf("}\n");
+}
+
+#endif
+
